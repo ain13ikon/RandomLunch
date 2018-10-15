aba4178db62c4fbe81c4722146f109af1685294f
ain <ain.git13@gmail.com>
2018-10-15 16:13:37 +0900

    Initial Commit

---
+++ RandomLunch2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomLunch2
+//
+//  Created by きたむら on 2018/10/15.
+//  Copyright © 2018年 ain13ikon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
